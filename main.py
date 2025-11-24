582375d29d0f55285a0902badfcb7e40ef452a95
kinoshitakenta <boxuan@techmation.com.tw>
2025-11-24 11:43:15 +0800

    feat: load game check list from json file

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+import json
+import os
+
 from modules.logger import setup_logging
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_DIR = os.path.join(BASE_DIR, "config")
+
+
+def load_json(filepath):
+    if not os.path.exists(filepath):
+        return {}
+    with open(filepath, 'r', encoding='utf-8') as f:
+        return json.load(f)
 
 
 def main():
     logger = setup_logging()
     logger.info("Starting Steam Update Monitor...")
+
+    try:
+        # * Load config info
+        games_config = load_json(os.path.join(CONFIG_DIR, "games.json"))
+
+    except FileNotFoundError as fnf_error:
+        logger.critical(f"Critical Error: {fnf_error}")
+        raise fnf_error
+
+    except Exception as e:
+        logger.exception("An unexpected error occurred during execution.")
 
     logger.info("Check finished.")
 
