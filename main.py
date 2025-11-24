3ba96e8fbcf2196bf968270e8a13213d5b87a0ee
kinoshitakenta <boxuan@techmation.com.tw>
2025-11-24 10:23:23 +0800

    feat: add logging module to track application runtime behavior
    Adds a logging module to track the application's execution.
    
    Introduced a structured logging system to improve observability, including:
    - Creation a `logs` directory for storing log files.
    - Configures a logger with both rotating file and console handlers.
    - Integrates logging throughout the main application workflow.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from modules.logger import setup_logging
+
+
 def main():
-    ...
+    logger = setup_logging()
+    logger.info("Starting Steam Update Monitor...")
+
+    logger.info("Check finished.")
 
 
 if __name__ == "__main__":
